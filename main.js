b6617f698ea6e7ba286fbe3a7004f64566cded17
mohamed eldeeb <73468629+eng-mohamed-eldeeb@users.noreply.github.com>
2022-02-14 20:58:25 +0200

    Add more style and Fix linter error

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // the book class
-class book {
+class Book {
   constructor(title, author) {
     this.title = title;
     this.author = author;
@@ -7,18 +7,18 @@ class book {
 }
 // to create and add the book
 function creatAndAddBooks(book, ul) {
-  //create list li elemnts
-  const li = document.createElement("li");
+  // create list li elemnts
+  const li = document.createElement('li');
   li.className = `${book.title}${book.author}`;
-  const pForTitle = document.createElement("p");
-  const pForAuthor = document.createElement("p");
-  const pForDelete = document.createElement("button");
-  const hr = document.createElement("hr");
-  pForDelete.className = "delete";
-  //add content to the elements
+  const pForTitle = document.createElement('p');
+  const pForAuthor = document.createElement('p');
+  const pForDelete = document.createElement('button');
+  const hr = document.createElement('hr');
+  pForDelete.className = 'delete';
+  // add content to the elements
   pForTitle.textContent = book.title;
   pForAuthor.textContent = book.author;
-  pForDelete.textContent = "Delete";
+  pForDelete.textContent = 'Delete';
   // add element to the list and the list to the ul
   li.appendChild(pForTitle);
   li.appendChild(pForAuthor);
@@ -26,29 +26,29 @@ function creatAndAddBooks(book, ul) {
   li.appendChild(hr);
   ul.appendChild(li);
 }
-//create the book list
+// create the book list
 const books = [];
-//call nedded elements from the document
-const bookList = document.querySelector(".book-list");
-const form = document.getElementById("abbBookForm");
-const titleVAlue = document.getElementById("title");
-const autherValue = document.getElementById("author");
-//create the books and add them the book list
-form.addEventListener("submit", (e) => {
+// call nedded elements from the document
+const bookList = document.querySelector('.book-list');
+const form = document.getElementById('abbBookForm');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+// create the books and add them the book list
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const newBooke = new book(title.value, author.value);
+  const newBooke = new Book(title.value, author.value);
   books.push(newBooke);
   creatAndAddBooks(newBooke, bookList);
 });
-//delete the book and remove them from the book list
-bookList.addEventListener("click", (e) => {
-  if (e.target.className == "delete") {
+// delete the book and remove them from the book list
+bookList.addEventListener('click', (e) => {
+  if (e.target.className === 'delete') {
     const li = e.target.parentElement;
     li.parentNode.removeChild(li);
-    for (let i = 0; i < books.length; i++) {
-      var ClassNAme = books[i].title;
+    for (let i = 0; i < books.length; i += 1) {
+      let ClassNAme = books[i].title;
       ClassNAme += books[i].author;
-      if (e.target.parentElement.className == ClassNAme) {
+      if (e.target.parentElement.className === ClassNAme) {
         books.splice(i, 1);
       }
     }
