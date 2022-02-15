dcf20fb20e8706e50b4e93067a350a14ccd77e2f
mohamed eldeeb <73468629+eng-mohamed-eldeeb@users.noreply.github.com>
2022-02-15 15:04:14 +0200

    Change the class to object

--- main.js
+++ main.js
@@ -1,31 +1,49 @@
 // the book class
-class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
+const Book = {
+    title: '',
+    author: '',
+    //to creat the title and the author
+    setBook: function (bookeTitle, bookeAuthor) {
+      this.title = bookeTitle;
+      this.author = bookeAuthor;
+    },
+    // to create and add the book
+    creatAndAddBooks: function (ul) {
+     // create list li elemnts
+      const li = document.createElement('li');
+      li.className = `${this.title}${this.author}`;
+      const pForTitle = document.createElement('p');
+      const pForAuthor = document.createElement('p');
+      const pForDelete = document.createElement('button');
+      const hr = document.createElement('hr');
+      pForDelete.className = 'delete';
+      // add content to the elements
+      pForTitle.textContent = this.title;
+      pForAuthor.textContent = this.author;
+      pForDelete.textContent = 'Delete';
+      // add element to the list and the list to the ul
+      li.appendChild(pForTitle);
+      li.appendChild(pForAuthor);
+      li.appendChild(pForDelete);
+      li.appendChild(hr);
+      ul.appendChild(li);
+  },
+
+  deleteBook: function (e) {
+    if (e.target.className === 'delete') {
+      const li = e.target.parentElement;
+      li.parentNode.removeChild(li);
+      for (let i = 0; i < books.length; i += 1) {
+        let ClassNAme = books[i].title;
+        ClassNAme += books[i].author;
+        if (e.target.parentElement.className === ClassNAme) {
+          books.splice(i, 1);
+        }
+      }
+    }
   }
 }
-// to create and add the book
-function creatAndAddBooks(book, ul) {
-  // create list li elemnts
-  const li = document.createElement('li');
-  li.className = `${book.title}${book.author}`;
-  const pForTitle = document.createElement('p');
-  const pForAuthor = document.createElement('p');
-  const pForDelete = document.createElement('button');
-  const hr = document.createElement('hr');
-  pForDelete.className = 'delete';
-  // add content to the elements
-  pForTitle.textContent = book.title;
-  pForAuthor.textContent = book.author;
-  pForDelete.textContent = 'Delete';
-  // add element to the list and the list to the ul
-  li.appendChild(pForTitle);
-  li.appendChild(pForAuthor);
-  li.appendChild(pForDelete);
-  li.appendChild(hr);
-  ul.appendChild(li);
-}
+
 // create the book list
 let books = [];
 if (localStorage.books) {
@@ -36,18 +54,26 @@ const bookList = document.querySelector('.book-list');
 const form = document.getElementById('abbBookForm');
 const title = document.getElementById('title');
 const author = document.getElementById('author');
+  const new_book = Book;
 // create the books, add them the book list and local storage
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const newBooke = new Book(title.value, author.value);
-  books.push(newBooke);
-  creatAndAddBooks(newBooke, bookList);
+  new_book.setBook(title.value, author.value);
+  new_book.creatAndAddBooks(bookList);
+  title.value = '';
+  author.value = '';
+  books.push(new_book);
   localStorage.books = JSON.stringify(books);
 });
 // create storaged books
-for (let i = 0; i < books.length; i += 1) {
-  const newBooke = new Book(books[i].title, books[i].author);
-  creatAndAddBooks(newBooke, bookList);
+for (let i = 1; i <= books.length; i += 1) {
+  // const new_book = Book;
+  new_book.setBook(books[i].title, books[i].author);
+  new_book.creatAndAddBooks(bookList);
+  console.log(books[i].title,books[i].author)
+  ///////
+  // const newBooke = new Book(books[i].title, books[i].author);
+  // creatAndAddBooks(newBooke, bookList);
 }
 // delete the book, remove them from the book list and local storage
 function deleteBook(e) {
@@ -65,6 +91,6 @@ function deleteBook(e) {
 }
 // call delatebook function by using the remove button
 bookList.addEventListener('click', (e) => {
-  deleteBook(e);
+  new_book.deleteBook(e);
   localStorage.books = JSON.stringify(books);
 });
