cabbb043a42abb72b09c3b834929d56795627c0d
mohamed eldeeb <73468629+eng-mohamed-eldeeb@users.noreply.github.com>
2022-02-16 19:59:11 +0200

    Fix the reviewer error #2

--- main.js
+++ main.js
@@ -1,59 +1,44 @@
-const titleValue = document.getElementById('title');
-const authorValue = document.getElementById('author');
-const booksUL = document.querySelector('.book-list');
+const titleValue = document.getElementById("title");
+const authorValue = document.getElementById("author");
+const booksUL = document.querySelector(".book-list");
 let bookArray = [];
-const form = document.querySelector('#abbBookForm');
+const form = document.querySelector("#abbBookForm");
 
 function show() {
   let bookId = 0;
-  let Current = localStorage.getItem('books');
+  let Current = localStorage.getItem("books");
   Current = JSON.parse(Current);
   if (Current) {
-    booksUL.innerHTML = '';
+    booksUL.innerHTML = "";
     Current.forEach((book) => {
-      const li = document.createElement('li');
-      const liP1 = document.createElement('li');
-      const liP2 = document.createElement('li');
+      const li = document.createElement("li");
+      const liP1 = document.createElement("li");
+      const liP2 = document.createElement("li");
       li.className = `${bookId}`;
-      const pForTitle = document.createElement('p');
-      const pForAuthor = document.createElement('p');
-      const ulPContaner = document.createElement('ul');
-      ulPContaner.className = 'ulWrapper';
-      const pForDelete = document.createElement('button');
-      pForDelete.className = 'delete';
-      // add content to the elements
+      const pForTitle = document.createElement("p");
+      const pForAuthor = document.createElement("p");
+      const ulPContaner = document.createElement("ul");
+      ulPContaner.className = "ulWrapper";
+      const pForDelete = document.createElement("button");
+      pForDelete.className = "delete";
       pForTitle.textContent = book.title;
       pForAuthor.textContent = book.author;
-      pForDelete.textContent = 'Remove';
-      // add element to the list and the list to the ul
+      pForDelete.textContent = "Remove";
       liP2.appendChild(pForTitle);
       liP1.appendChild(pForAuthor);
       ulPContaner.innerHTML = `"${book.title}" by ${book.author}`;
-      // ulPContaner.appendChild(liP1);
-      // ulPContaner.appendChild(liP2);
       li.appendChild(ulPContaner);
-      // li.appendChild(pForTitle);
-      // li.appendChild(pForAuthor);
       li.appendChild(pForDelete);
       booksUL.appendChild(li);
       if (bookId % 2 === 1) {
-        li.id = 'bg-color';
+        li.id = "bg-color";
       }
       bookId += 1;
     });
   }
 }
 
-function deleteBook(i) {
-  let currentBooks = localStorage.getItem('books');
-  currentBooks = JSON.parse(currentBooks);
-  currentBooks.splice(i, 1);
-  currentBooks = JSON.stringify(currentBooks);
-  localStorage.setItem('books', currentBooks);
-  show();
-}
-
-if (localStorage.getItem('books') != null) {
+if (localStorage.getItem("books") != null) {
   show();
 }
 class Book {
@@ -63,28 +48,37 @@ class Book {
   }
 
   addToLocalStorage() {
-    const Current = localStorage.getItem('books');
+    const Current = localStorage.getItem("books");
     if (!Current) {
       bookArray.push(this);
       const newList = JSON.stringify(bookArray);
-      localStorage.setItem('books', newList);
+      localStorage.setItem("books", newList);
     } else {
       bookArray = JSON.parse(Current);
       bookArray.push(this);
       const newList = JSON.stringify(bookArray);
-      localStorage.setItem('books', newList);
+      localStorage.setItem("books", newList);
     }
   }
+
+  deleteBook(i) {
+    let currentBooks = localStorage.getItem("books");
+    currentBooks = JSON.parse(currentBooks);
+    currentBooks.splice(i, 1);
+    currentBooks = JSON.stringify(currentBooks);
+    localStorage.setItem("books", currentBooks);
+    show();
+  }
 }
-form.addEventListener('submit', (e) => {
+const newBook = new Book(titleValue.value, authorValue.value);
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const newBook = new Book(titleValue.value, authorValue.value);
   newBook.addToLocalStorage();
   show();
-  titleValue.value = '';
-  authorValue.value = '';
+  titleValue.value = "";
+  authorValue.value = "";
 });
 
-booksUL.addEventListener('click', (e) => {
-  deleteBook(e.target.parentNode.className);
+booksUL.addEventListener("click", (e) => {
+  newBook.deleteBook(e.target.parentNode.className);
 });
